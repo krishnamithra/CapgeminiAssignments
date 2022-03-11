60e8070b63bd54ed64722ba35d48f5d63fe64b12
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 09:26:22 +0530

    Create TestEmployee.java

---
+++ Day-2/TestEmployee.java
@@ -0,0 +1,13 @@
+package day2;
+
+public class TestEmployee {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee employee = new Employee("John Doe", 5000000, 2019, "321a2sd1321ad");
+        System.out.println(employee);
+
+
+	}
+
+}
