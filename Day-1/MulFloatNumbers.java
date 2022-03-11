31c3b62863b5752a3817038d3cb8a0be65e5b812
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 09:15:33 +0530

    Create MulFloatNumbers.java

---
+++ Day-1/MulFloatNumbers.java
@@ -0,0 +1,18 @@
+package javademo;
+import java.util.Scanner;
+
+public class MulFloatNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		float a = s.nextFloat();
+		float b = s.nextFloat();
+		
+		double result = a*b;
+		
+		System.out.println("The product is: " + result);
+
+	}
+
+}
