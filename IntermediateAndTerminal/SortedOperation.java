6da0eef12be3e184e2ba60c6e634f87e5c5c9254
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 10:11:57 +0530

    Update SortedOperation.java

--- IntermediateAndTerminal/SortedOperation.java
+++ IntermediateAndTerminal/SortedOperation.java
@@ -8,14 +8,9 @@ public class SortedOperation {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		List<String> list = new ArrayList<String>();
-		list.add("Rohit");
-		list.add("Oppoppi");
-		list.add("YEEHAWWWW?");
+		list.add("Krishna");
 		list.add("Hello");
 		list.add("Hi");
-		list.add("Right");
-		list.add("Hilarious");
-		list.add("Dull");
 		
 		list.stream().sorted().map(String::toLowerCase).forEach(System.out::println);
 
