b75e01de274b8c1c9dd681eeaa5d83b83e9d8e76
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 10:02:01 +0530

    Create Subtraction.java

---
+++ Functional Interface and Lambda Expressions with Optionals/Subtraction.java
@@ -0,0 +1,35 @@
+package FunctionalInterfaceAndLambda;
+
+import java.util.Optional;
+
+interface Finder1{
+	public double value(Optional<Double> a,Optional<Double> b);
+}
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Finder f= (a,b) ->{
+			
+			
+			Double a1 = a.orElse((double) 0);
+			Double b1 = b.get();
+			
+			return a1-b1;
+			
+			
+			
+		};
+		Double value1 = 12.0;
+		Double value2 = new Double(8.0);
+		Optional<Double> a = Optional.ofNullable(value1);
+		Optional<Double> b = Optional.of(value2);
+		
+		System.out.println(f.value(a, b));
+		
+
+	}
+
+}
