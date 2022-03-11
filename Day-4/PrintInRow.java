26fd5420727cb591f7040a6295fbd76053b46736
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 09:41:01 +0530

    Create PrintInRow.java

---
+++ Day-4/PrintInRow.java
@@ -0,0 +1,13 @@
+package day4;
+
+public class PrintInRow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++) {
+			System.out.print(i + "    ");
+		}
+
+	}
+
+}
