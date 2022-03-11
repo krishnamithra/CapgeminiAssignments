709ae1b1049500fac37b66aa3ca3e7f259efd2bf
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 09:43:38 +0530

    Create UpperLower.java

---
+++ Day-4/UpperLower.java
@@ -0,0 +1,20 @@
+package day4;
+import java.util.Scanner;
+
+
+public class UpperLower {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		char a=s.next().charAt(0);
+        char b;
+        if(Character.isLowerCase(a))
+            b= Character.toUpperCase(a);
+        else
+            b= Character.toLowerCase(a);
+        System.out.println(a+"->"+b);
+
+	}
+
+}
