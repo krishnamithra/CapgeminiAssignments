4ce44d82412f1810dc3772a01b8d7143979bda0c
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 09:17:56 +0530

    Create displayInteger.java

---
+++ Day-1/displayInteger.java
@@ -0,0 +1,17 @@
+package javademo;
+
+import java.util.Scanner;
+
+public class displayInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int result = s.nextInt();
+		
+		System.out.println("The integer entered is: " + result);
+		
+
+	}
+
+}
