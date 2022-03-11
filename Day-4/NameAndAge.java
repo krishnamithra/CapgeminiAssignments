9713c0d8dc86dbce1779876c7babd0e8e4c6cbb2
krishnamithra <59324555+krishnamithra@users.noreply.github.com>
2022-03-11 09:37:29 +0530

    Create NameAndAge.java

---
+++ Day-4/NameAndAge.java
@@ -0,0 +1,16 @@
+package day4;
+
+public class NameAndAge {
+
+	public static void main(String[] args) throws InvalidAgeException {
+		String name = args[0];
+		
+		int age = Integer.parseInt(args[1]);
+		
+		if (age < 18 || age >= 60)
+			throw new InvalidAgeException();
+		
+		System.out.println("Name: " + name + " Age: " + age);
+	}
+
+}
